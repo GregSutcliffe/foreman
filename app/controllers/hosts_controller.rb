57664d2427308eca0805427a26973b18277f1557
Joseph Mitchell Magen <jmagen@redhat.com>
2013-04-09 17:03:12 +0300

    serialized whole form in post to puppetclass_parameters and used params['host'] rather than retrieving from database

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -472,8 +472,11 @@ class HostsController < ApplicationController
   private
 
   def refresh_host
-    @host = Host::Base.find_by_id(params['host_id'])
-    @host ||= Host::Managed.new
+    if params['host'].present?
+      @host = Host::Managed.new(params['host'])
+    else
+      @host = Host::Base.find_by_id(params['host_id'])
+    end
     # If we found a host from another STI type, convert it so we can call Managed methods on it
     unless @host.class.name == "Host::Managed"
       @host      = @host.becomes(Host::Managed)
