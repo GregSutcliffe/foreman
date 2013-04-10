ba49e50070474cf01264b291af880d56ceee6c6a
Greg Sutcliffe <gsutclif@redhat.com>
2013-04-10 09:55:44 +0100

    Move Host.new down a line to make sure it's always checked

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -476,8 +476,8 @@ class HostsController < ApplicationController
       @host = Host::Managed.new(params['host'])
     else
       @host = Host::Base.find_by_id(params['host_id'])
-      @host ||= Host::Managed.new
     end
+    @host ||= Host::Managed.new
     # If we found a host from another STI type, convert it so we can call Managed methods on it
     unless @host.class.name == "Host::Managed"
       @host      = @host.becomes(Host::Managed)
