4eaa1fc13205b443961521b107ad2a5671d2209d
Greg Sutcliffe <gsutclif@redhat.com>
2014-09-12 11:32:14 +0100

    WIP convert host fixtures to factories

--- test/unit/suse_test.rb
+++ test/unit/suse_test.rb
@@ -2,6 +2,24 @@ require 'test_helper'
 
 class SuseTest < ActiveSupport::TestCase
 
+  #-suse:
+  #-  type: Host::Managed
+  #-  name: suse01.yourdomain.net
+  #-  ip: 2.3.4.205
+  #-  mac: ad:bb:cc:da:ae:ff
+  #-  environment: production
+  #-  architecture: x86_64
+  #-  build: true
+  #-  ptable: suse
+  #-  domain: yourdomain
+  #-  medium: suse
+  #-  operatingsystem: suse
+  #-  subnet: one
+  #-  puppet_proxy: puppetmaster
+  #-  compute_resource: one
+  #-  root_pass: xybxa6JUkz63w
+  #-  provision_method: build
+
   test "os label for suse" do
     assert_equal "OpenSuse 11.4", operatingsystems(:suse).to_label
   end
@@ -11,23 +29,29 @@ class SuseTest < ActiveSupport::TestCase
   end
 
   test "kernel location for 64bit arch suse" do
-    host = hosts(:suse)
+    host = FactoryGirl.create(:host, :operatingsystem => operatingsystems(:suse),
+                              :architecture => architectures(:x86_64))
     assert_equal "boot/OpenSuse-11.4-x86_64-linux", host.os.kernel(host.arch)
   end
 
   test "initrd location for 64bit arch suse" do
-    host = hosts(:suse)
+    host = FactoryGirl.create(:host, :operatingsystem => operatingsystems(:suse),
+                              :architecture => architectures(:x86_64))
     assert_equal "boot/OpenSuse-11.4-x86_64-initrd", host.os.initrd(host.arch)
   end
 
   test "pxe prefix for suse" do
-    host = hosts(:suse)
+    host = FactoryGirl.create(:host, :operatingsystem => operatingsystems(:suse),
+                              :architecture => architectures(:x86_64))
     prefix = host.os.pxe_prefix(host.arch)
     assert_equal "boot/OpenSuse-11.4-x86_64", prefix
   end
 
   test "pxe files for suse" do
-    host = hosts(:suse)
+    host = FactoryGirl.create(:host, :with_medium,
+                              :operatingsystem => operatingsystems(:suse),
+                              :architecture => architectures(:x86_64),
+                              :medium => media(:suse) )
     host.medium.operatingsystems << host.os
     host.arch.operatingsystems << host.os
 
